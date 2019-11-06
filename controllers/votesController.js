f9f259c07b88cf345f929c0b9cbda5a4c6521233
Tambourin <jirka.hartonen@gmail.com>
2019-11-06 14:01:31 +0200

    controllerien parantelua

--- controllers/votesController.js
+++ controllers/votesController.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const Place = require("../models/placeModel");
-
+const googleService = require("../services/googleService");
 
 router.post("/:placeId/votes", async (request, response) => {
   try {
     const place = await Place.findById(request.params.placeId);
     place.votes += 1;
     place.save(); 
-    return response.send(place.toObject());
+    response.send(await googleService.appendSinglePlace(place.toObject()));
   } catch (exception) {
     console.log(exception.message);
     response.status(500).send({ error: exception.message });
@@ -19,7 +19,7 @@ router.delete("/:placeId/votes", async (request, response) => {
     const place = await Place.findById(request.params.placeId);
     place.votes -= 1;
     place.save();
-    return response.send(place.toObject());
+    response.send(await googleService.appendSinglePlace(place.toObject()));
   } catch (exception) {
     console.log(exception.message);
     response.status(500).send({ error: exception.message });
